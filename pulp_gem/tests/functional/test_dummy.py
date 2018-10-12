16cbaf2c127eeb0bba72e1967730e6110c815761
Matthias Dellweg <dellweg@atix.de>
2018-10-12 14:36:53 +0200

    Add tests framework

---
+++ pulp_gem/tests/functional/test_dummy.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class DummyTestCase(unittest.TestCase):
+    """To be improved."""
+
+    def test_the_truth(self):
+        """To be improved."""
+        self.assertEqual(1 + 1, 2)
