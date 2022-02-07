301d1ae372efdd7253ff859ff17f9317044fd103
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-02-07 12:53:38 +0000

    Ensure sync is async safe
    [noissue]

--- pulp_gem/app/tasks/synchronizing.py
+++ pulp_gem/app/tasks/synchronizing.py
@@ -4,6 +4,8 @@ import os
 from gettext import gettext as _
 from urllib.parse import urlparse, urlunparse
 
+from asgiref.sync import sync_to_async
+
 from pulpcore.plugin.models import Artifact, ProgressReport, Remote, Repository
 from pulpcore.plugin.stages import (
     DeclarativeArtifact,
@@ -46,9 +48,9 @@ class UpdateExistingContentArtifacts(Stage):
         async for d_content in self.items():
             if d_content.content.pk is not None:
                 for d_artifact in d_content.d_artifacts:
-                    content_artifact = d_content.content.contentartifact_set.get(
-                        relative_path=d_artifact.relative_path
-                    )
+                    content_artifact = await sync_to_async(
+                        d_content.content.contentartifact_set.select_related("artifact").get
+                    )(relative_path=d_artifact.relative_path)
                     if content_artifact.artifact is not None:
                         d_artifact.artifact = content_artifact.artifact
             await self.put(d_content)
@@ -100,16 +102,16 @@ class GemFirstStage(Stage):
         # Interpret policy to download Artifacts or not
         deferred_download = self.remote.policy != Remote.IMMEDIATE
 
-        with ProgressReport(message="Downloading Metadata") as progress:
+        async with ProgressReport(message="Downloading Metadata") as progress:
             parsed_url = urlparse(self.remote.url)
             root_dir = parsed_url.path
             specs_path = os.path.join(root_dir, "specs.4.8.gz")
             specs_url = urlunparse(parsed_url._replace(path=specs_path))
             downloader = self.remote.get_downloader(url=specs_url)
             result = await downloader.run()
-            progress.increment()
+            await progress.aincrement()
 
-        with ProgressReport(message="Parsing Metadata") as progress:
+        async with ProgressReport(message="Parsing Metadata") as progress:
             for key in read_specs(result.path):
                 relative_path = os.path.join("gems", key.name + "-" + key.version + ".gem")
                 path = os.path.join(root_dir, relative_path)
@@ -136,7 +138,7 @@ class GemFirstStage(Stage):
                     deferred_download=deferred_download,
                 )
                 dc = DeclarativeContent(content=gem, d_artifacts=[da_gem, da_spec])
-                progress.increment()
+                await progress.aincrement()
                 await self.put(dc)
 
 
