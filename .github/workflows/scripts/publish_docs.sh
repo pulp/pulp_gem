cb0b4492f712ad60da365a34bda640181c8c44e1
pulpbot <pulp-infra@redhat.com>
2022-03-25 12:38:18 +0000

    Update CI files
    [noissue]

--- .github/workflows/scripts/publish_docs.sh
+++ .github/workflows/scripts/publish_docs.sh
@@ -37,19 +37,11 @@ if [[ "$GITHUB_WORKFLOW" == "Gem changelog update" ]]; then
   exit
 fi
 
-# Building python bindings
-export PULP_URL="${PULP_URL:-https://pulp}"
-VERSION=$(http $PULP_URL/pulp/api/v3/status/ | jq --arg plugin gem --arg legacy_plugin pulp_gem -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
-cd ../pulp-openapi-generator
-rm -rf pulp_gem-client
-./generate.sh pulp_gem python $VERSION
-cd pulp_gem-client
+pip install mkdocs pymdown-extensions "Jinja2<3.1"
 
-# Adding mkdocs
-find ./docs/* -exec sed -i 's/README//g' {} \;
-cp README.md docs/index.md
-sed -i 's/docs\///g' docs/index.md
-find ./docs/* -exec sed -i 's/\.md//g' {} \;
+mkdir -p ../bindings
+tar -xvf python-client-docs.tar --directory ../bindings
+cd ../bindings
 cat >> mkdocs.yml << DOCSYAML
 ---
 site_name: PulpGem Client
@@ -61,8 +53,6 @@ repo_url: https://github.com/pulp/pulp_gem
 theme: readthedocs
 DOCSYAML
 
-pip install mkdocs pymdown-extensions
-
 # Building the bindings docs
 mkdocs build
 
@@ -70,4 +60,4 @@ mkdocs build
 rsync -avzh site/ doc_builder_pulp_gem@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulp_gem_client/
 
 # publish to docs.pulpproject.org/pulp_gem_client/en/{release}
-rsync -avzh site/ doc_builder_pulp_gem@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulp_gem_client/en/"$1"
+rsync -avzh site/ doc_builder_pulp_gem@docs.pulpproject.org:/var/www/docs.pulpproject.org/pulp_gem_client/en/"$2"
