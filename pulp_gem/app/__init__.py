bd203bcb0902cffcd6a80872eafb3772a129fb1c
pulpbot <pulp-infra@redhat.com>
2023-06-26 17:52:48 +0000

    Release 0.1.0 GH Issues: 94,107,96,101,99,93
    [noissue]

--- pulp_gem/app/__init__.py
+++ pulp_gem/app/__init__.py
@@ -6,5 +6,5 @@ class PulpGemPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_gem.app"
     label = "gem"
-    version = "0.1.0.dev"
+    version = "0.1.0"
     python_package_name = "pulp-gem"
