c335cc345d406b935ccbad6f812703663b6e135b
pulpbot <pulp-infra@redhat.com>
2023-09-18 14:54:44 +0000

    Bump to 0.3.1.dev
    [noissue]

--- pulp_gem/app/__init__.py
+++ pulp_gem/app/__init__.py
@@ -6,5 +6,5 @@ class PulpGemPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_gem.app"
     label = "gem"
-    version = "0.3.0"
+    version = "0.3.1.dev"
     python_package_name = "pulp-gem"
