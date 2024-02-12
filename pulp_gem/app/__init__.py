ef29e0c87525eaf06806c75ce1146c5005a28143
pulpbot <pulp-infra@redhat.com>
2024-02-12 17:40:06 +0000

    Bump minor version [noissue]

--- pulp_gem/app/__init__.py
+++ pulp_gem/app/__init__.py
@@ -6,6 +6,6 @@ class PulpGemPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_gem.app"
     label = "gem"
-    version = "0.5.0.dev"
+    version = "0.6.0.dev"
     python_package_name = "pulp-gem"
     domain_compatible = True
