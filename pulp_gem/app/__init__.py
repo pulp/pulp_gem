6b625bb1975b3cee822952fb3eedf33e03a167e5
pulpbot <pulp-infra@redhat.com>
2024-06-19 11:21:23 +0000

    Bump version: 0.6.0 → 0.6.1.dev

--- pulp_gem/app/__init__.py
+++ pulp_gem/app/__init__.py
@@ -6,6 +6,6 @@ class PulpGemPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_gem.app"
     label = "gem"
-    version = "0.6.0"
+    version = "0.6.1.dev"
     python_package_name = "pulp-gem"
     domain_compatible = True
