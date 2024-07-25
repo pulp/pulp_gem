b1a5bfeb53ac159c2359baa2983ef4108b193fcb
Pedro Brochado <pedropsb95@gmail.com>
2024-07-25 12:45:01 -0300

    Rename staging_docs to docs
    This does:
    
    - Remove legacy docs
    
    - Rename staging_docs to docs
    
    [noissue]

--- docs/_scripts/artifact.sh
+++
@@ -1,7 +0,0 @@
-# Get a Gem package or choose your own
-curl -O https://fixtures.pulpproject.org/gem/gems/amber-1.0.0.gem
-
-# Upload it to Pulp
-pulp gem content upload --file "amber-1.0.0.gem"
-
-CONTENT_HREF=$(pulp gem content list | jq -r .[0].pulp_href)
--- docs/_scripts/pullthrough.sh
+++
@@ -1,2 +0,0 @@
-# Add remote to distribution to enable pull-through caching
-pulp gem distribution update --name foo --remote gem
