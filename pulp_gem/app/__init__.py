4fcef59e2b5b6fac656e56ab766658714281b8ae
pulpbot <pulp-infra@redhat.com>
2025-02-10 16:08:17 +0000

    Bump version: 0.6.2 → 0.6.3.dev

--- pulp_gem/app/__init__.py
+++ pulp_gem/app/__init__.py
@@ -6,6 +6,6 @@ class PulpGemPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_gem.app"
     label = "gem"
-    version = "0.6.2"
+    version = "0.6.3.dev"
     python_package_name = "pulp-gem"
     domain_compatible = True
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as f:
 
 setup(
     name="pulp-gem",
-    version="0.6.2",
+    version="0.6.3.dev",
     description="Gemfile plugin for the Pulp Project",
     long_description=long_description,
     license="GPLv2+",
